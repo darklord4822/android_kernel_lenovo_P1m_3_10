d0c9f6b22f0f3eb51ed3d85b24f4eda6eddf2294
darklord4822 <darklord4722@gmail.com>
2017-01-31 16:53:01 +0000

    blabla

--- drivers/misc/mediatek/gpu/ged/include/ged_base.h
+++ drivers/misc/mediatek/gpu/ged/include/ged_base.h
@@ -1,15 +1,15 @@
 #ifndef __GED_BASE_H__
 #define __GED_BASE_H__
 
-#include <linux/xlog.h>
+#include <linux/compiler.h>
 #include "ged_type.h"
 
 #ifdef GED_DEBUG
-#define GED_LOGI(...)	xlog_printk(ANDROID_LOG_INFO, "GED", __VA_ARGS__)
+#define GED_LOGI(...)	pr_debug("GED:" __VA_ARGS__)
 #else
 #define GED_LOGI(...)
 #endif
-#define GED_LOGE(...)	xlog_printk(ANDROID_LOG_ERROR, "GED", __VA_ARGS__)
+#define GED_LOGE(...)	pr_debug("GED:" __VA_ARGS__)
 #define GED_CONTAINER_OF(ptr, type, member) ((type *)( ((char *)ptr) - offsetof(type,member) ))
 
 unsigned long ged_copy_to_user(void __user *pvTo, const void *pvFrom, unsigned long ulBytes);
